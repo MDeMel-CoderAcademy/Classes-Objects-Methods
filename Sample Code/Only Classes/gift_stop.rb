caa3ff627e532a8b139a7333900fd031ffcf592e
MDeMel-CoderAcademy <83929816+MDeMel-CoderAcademy@users.noreply.github.com>
2021-05-18 21:49:22 +1000

    Only Classes
    Sample Code part 1 - for classes , objects and methods review

---
+++ Sample Code/Only Classes/gift_stop.rb
@@ -0,0 +1,24 @@
+require_relative 'word_splitter'
+require_relative 'giftbox'
+
+class GiftStop
+
+     @gift_types = %w/electric sweets drinks books apparel/
+     @persons = %w/Alex Iryna Bella Steffano Yujing/
+
+     @visited_persons = []
+     @gifts_given = []
+
+        count = 0
+        while count < 5
+           @visited_persons << Person.new
+           @visited_persons[count].name = @persons[count]
+           @gifts_given << GiftBox.new(id: count ,description: @gift_types[count] )
+           count += 1
+         end
+
+      @visited_persons.each_with_index { |person, idx| puts "#{person.name} was given a #{@gifts_given[idx].description}" }
+
+
+
+ end
---
+++ Sample Code/Only Classes/giftbox.rb
@@ -0,0 +1,11 @@
+class GiftBox
+     attr_accessor    :id , :description
+
+        def initialize (id: , description: "electronic")
+
+        @id = id
+        @description   =  description
+
+         end
+
+      end
